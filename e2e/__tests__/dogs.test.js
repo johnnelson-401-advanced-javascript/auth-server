58f3e360b948c62c65e7acf0d93a44ab6605afa3
JOHN NELSON <JohnNelson4850@gmail.com>
2019-10-04 14:41:58 -0700

    Updates a Doggo

--- e2e/__tests__/dogs.test.js
+++ e2e/__tests__/dogs.test.js
@@ -32,7 +32,6 @@ describe('Tests the Dogs routes', () => {
   }
   it('posts a doggo', () => {
     return postDoggo(testDoggo).then(body => {
-      // console.log(res);
       expect(body.owner).toBe(user._id);
       expect(body).toMatchInlineSnapshot(
         {
@@ -58,6 +57,47 @@ describe('Tests the Dogs routes', () => {
         }
       `
       );
+    });
+  });
+  it('Updates a Doggo using the Put route, Only Owner can update', () => {
+    function updateDoggo(dog, update) {
+      return request
+        .put(`/api/dogs/${dog._id}`)
+        .set('Authorization', user.token)
+        .send(update)
+        .expect(200)
+        .then(({ body }) => body);
+    }
+
+    let puppyUpdate = { hasPuppies: 5 };
+
+    return postDoggo(testDoggo).then(body => {
+      return updateDoggo(body, puppyUpdate).then(body => {
+        expect(body).toMatchInlineSnapshot(
+          {
+            ...body,
+            _id: expect.any(String),
+            owner: expect.any(String),
+            hasPuppies: 5
+          },
+          `
+          Object {
+            "__v": 0,
+            "_id": Any<String>,
+            "appearances": Object {
+              "breed": "husky",
+              "mainColor": "red",
+            },
+            "hasPuppies": 5,
+            "isGoodDog": true,
+            "media": Array [],
+            "name": "Togo",
+            "owner": Any<String>,
+            "yearIntroduced": 1913,
+          }
+        `
+        );
+      });
     });
   });
 });
